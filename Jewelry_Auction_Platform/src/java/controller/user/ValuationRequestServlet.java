b3be9d1d2e801b4ef4b81a0643a175f4f360f304
Bennguyen2801 <bennsse184229@fpt.edu.vn>
2024-05-19 18:28:32 +0700

    Modify send valuation request to admin

--- Jewelry_Auction_Platform/src/java/controller/user/ValuationRequestServlet.java
+++ Jewelry_Auction_Platform/src/java/controller/user/ValuationRequestServlet.java
@@ -4,13 +4,24 @@
  */
 package controller.user;
 
+import dao.UserDAO;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  *
@@ -18,6 +29,9 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "ValuationRequestServlet", urlPatterns = {"/ValuationRequestServlet"})
 public class ValuationRequestServlet extends HttpServlet {
+
+    private static final String ERROR_PAGE = "/WEB-INF/jsp/index.jsp";
+    private static final String HOME_PAGE = "home.jsp";
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -32,16 +46,69 @@ public class ValuationRequestServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ValuationRequestServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ValuationRequestServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            String url = ERROR_PAGE; 
+            boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+            try {
+                if (!isMultipart) {
+                } else {
+                    FileItemFactory factory = new DiskFileItemFactory();
+                    ServletFileUpload upload = new ServletFileUpload(factory);
+                    List items = null;
+                    try {
+                        items = upload.parseRequest(request);
+                    } catch (FileUploadException e) {
+                        e.printStackTrace();
+                    }
+                    Iterator iter = items.iterator();
+                    Hashtable params = new Hashtable();
+                    String fileName = null;
+                    while (iter.hasNext()) {
+                        FileItem item = (FileItem) iter.next();
+                        if (item.isFormField()) {
+                            params.put(item.getFieldName(), item.getString());
+                        } else {
+                            try {
+                                String itemName = item.getName();
+                                fileName = itemName.substring(itemName.lastIndexOf("\\") + 1);
+
+                                // Tạo thư mục images nếu chưa tồn tại
+                                File imageDir = new File(getServletContext().getRealPath("/") + "images");
+                                if (!imageDir.exists()) {
+                                    imageDir.mkdirs();
+                                }
+
+                                String RealPath = getServletContext().getRealPath("/") + "images\\" + fileName;
+                                File savedFile = new File(RealPath);
+                                item.write(savedFile);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }//end while
+                    //get Parameter
+                    
+                    String name = (String) params.get("name");
+                    String email = (String) params.get("email");
+                    String phone = (String) params.get("phone");
+                    String communication = (String) params.get("communication");
+                    String photos = "images/" + fileName;
+                    String description = (String) params.get("description");
+                    try {
+                        UserDAO dao = new UserDAO();
+                        boolean result = dao.sendValuationRequest(name, email, phone, communication, photos, "U0");
+                        if (result) {
+                            url = HOME_PAGE;
+                        }
+                    } catch (Exception ex) {
+                        ex.getMessage();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                RequestDispatcher dist = request.getRequestDispatcher(url);
+                dist.forward(request, response);
+            }
         }
     }
 
