ed3a7d36a7b8e6960232102e3458e80affe660d8
ThangVVSE180202 <thangvvse180202@fpt.edu.vn>
2024-05-19 12:01:36 +0700

    Delete test.js

--- Jewelry_Auction_Platform/web/component/test.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('bell-icon').addEventListener('click', function() {
-    var bellBox = document.getElementById('bell-box');
-    if (bellBox.style.display === 'none') {
-        bellBox.style.display = 'block';
-    } else {
-        bellBox.style.display = 'none';
-    }
-});
-
