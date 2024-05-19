85cf263e446f11e6a8660a124e973bf05743e156
Bennguyen2801 <bennsse184229@fpt.edu.vn>
2024-05-19 10:27:59 +0700

    Add valuation.jsp and modify pages

--- Jewelry_Auction_Platform/src/java/utils/DBUtils.java
+++ Jewelry_Auction_Platform/src/java/utils/DBUtils.java
@@ -11,16 +11,16 @@ import java.sql.SQLException;
 
 /**
  *
- * @author User
+ * @author Hoa Doan
  */
 public class DBUtils {
-
+//    Do not change this code
     private static final String DB_NAME = "JewelryAuctionSystem";
     private static final String DB_USER_NAME = "SA";
     private static final String DB_PASSWORD = "12345";
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
-        Connection conn;
+        Connection conn = null;
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         String url = "jdbc:sqlserver://localhost:1433;databaseName=" + DB_NAME;
         conn = DriverManager.getConnection(url, DB_USER_NAME, DB_PASSWORD);
