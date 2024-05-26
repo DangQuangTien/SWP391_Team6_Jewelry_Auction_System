aa310cc1828f197ff8bfb6e9372e9df3f383b5c9
DangQuangTien <tiendqse171443@fpt.edu.vn>
2024-05-26 12:59:23 +0700

    Reapply "entity class"
    This reverts commit 3aaf51e5fa1dca2419f5014a7e217d57adccdfd1.

---
+++ Jewelry_Auction_Platform/src/java/entity/Staff.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author T14
+ */
+@Entity
+@Table(name = "Staff")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Staff.findAll", query = "SELECT s FROM Staff s"),
+    @NamedQuery(name = "Staff.findByStaffID", query = "SELECT s FROM Staff s WHERE s.staffID = :staffID"),
+    @NamedQuery(name = "Staff.findByFullName", query = "SELECT s FROM Staff s WHERE s.fullName = :fullName")})
+public class Staff implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "staffID")
+    private String staffID;
+    @Size(max = 255)
+    @Column(name = "fullName")
+    private String fullName;
+    @JoinColumn(name = "userID", referencedColumnName = "userID")
+    @ManyToOne(optional = false)
+    private Users userID;
+
+    public Staff() {
+    }
+
+    public Staff(String staffID) {
+        this.staffID = staffID;
+    }
+
+    public String getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(String staffID) {
+        this.staffID = staffID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Users getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Users userID) {
+        this.userID = userID;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (staffID != null ? staffID.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Staff)) {
+            return false;
+        }
+        Staff other = (Staff) object;
+        if ((this.staffID == null && other.staffID != null) || (this.staffID != null && !this.staffID.equals(other.staffID))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Staff[ staffID=" + staffID + " ]";
+    }
+    
+}
